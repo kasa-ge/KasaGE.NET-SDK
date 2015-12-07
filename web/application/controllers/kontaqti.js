bb24141b2e3f8773801f7c6758827777f962aa86
Vaso Bolkvadze <vaso.bolkvadze@gmail.com>
2015-12-07 15:19:02 +0400

    davamate mxardachera da gadmosaceri linkebi

--- web/application/controllers/kontaqti.js
+++ web/application/controllers/kontaqti.js
@@ -1,6 +1,6 @@
 module.exports = function (router) {
 	router.get('/kontaqti'
 		, function (req, res, next) {
-			res.render('kontaqti/main');
+				res.render('kontaqti/main');
 		});
 };
---
+++ web/application/controllers/mxardachera.js
@@ -0,0 +1,6 @@
+module.exports = function (router) {
+	router.get('/mxardachera/programuli'
+		, function (req, res, next) {
+				res.render('mxardachera/programuli');
+		});
+};
