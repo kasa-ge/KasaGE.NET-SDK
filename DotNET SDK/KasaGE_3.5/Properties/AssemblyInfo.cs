98e1a0cbf553a8810749236a46aa68989d081f02
Kasa.ge <info@kasa.ge>
2015-12-11 14:23:45 +0400

    ADD: 3.5 Project xml doc file, assembly info

--- DotNET SDK/KasaGE_3.5/Properties/AssemblyInfo.cs
+++ DotNET SDK/KasaGE_3.5/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KasaGE_3.5")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("KasaGE")]
+[assembly: AssemblyDescription("ECR SDK")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KasaGE_3.5")]
+[assembly: AssemblyCompany("Vaso Bolkvadze")]
+[assembly: AssemblyProduct("ECR SDK")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
